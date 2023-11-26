e802cd413266c8876fd2cec1698559c188a996a3
JAANUS SRI K G <jaanussri2110611@ssn.edu.in>
2023-11-27 00:51:03 +0530

    Initial commit of the Factorial App

---
+++ src/main/java/com/SoftwareEngineering/FactorialApp/FactorialAppApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.FactorialApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftwareEngineering/FactorialApp/FactorialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.FactorialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
